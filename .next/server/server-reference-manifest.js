c973a3e2014e7b52ee585a7d7550cc83553c9333
Latesh-31 <latesh312006@gmail.com>
2025-12-05 11:39:00 +0530

    cc

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {}\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"sh40Apf6l4/FKzRWDEVS0ERbVQWMC/AKXPi0O3hEYRw=\"\n}"
